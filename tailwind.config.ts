2cc6366acb7e35fc6dc4ff34e5b3423f0a01f332
Fernando Bade <bade.fernando@gmail.com>
2024-07-23 22:37:23 -0300

    fontes e base de cor Dracula

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./public/index.html', './src/**/*.{html,ts,tsx,js,jsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('@tailwindcss/forms')],
+    content: ['./public/index.html', './src/**/*.{html,ts,tsx,js,jsx}'],
+    theme: {
+        extend: {
+            colors: {
+                fundo: '#282a36',
+                offwhite: '#f8f8f2',
+                cinzaAuzulado: '#6272a4',
+                cinza: '#44475a',
+                ciano: '#8be9fd',
+                verde: '#50fa7b',
+                laranja: '#ffb86c',
+                rosa: '#ff79c6',
+                roxo: '#bd93f9',
+                vermelho: '#ff5555',
+                amarelo: '#f1fa8c',
+            },
+            fontFamily: {
+                principal: ['Zain', 'sans-serif'],
+                secundaria: ['Outfit', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [require('@tailwindcss/forms')],
 };
